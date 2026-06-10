9bea7681d8ca679952ad3bf1d91bf5d148a4dc58
FARIS JM <faris.mjm2004@gmail.com>
2026-06-10 10:54:22 +0530

    Implement student addition feature
    Added functionality to add a student with ID, name, and marks.

--- StudentGradeManagementSystem.java
+++ StudentGradeManagementSystem.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 class Student {
     String id;
     String name;
@@ -9,7 +12,26 @@ class Student {
         this.marks = marks;
     }
 }
+public class StudentGradeManagementSystem {
 
+    static Student[] students = new Student[100];
+    static int count = 0;
+
+    public static void addStudent(Scanner sc) {
+        System.out.print("Enter ID: ");
+        String id = sc.nextLine();
+
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Marks: ");
+        double marks = sc.nextDouble();
+        sc.nextLine();
+
+        students[count++] = new Student(id, name, marks);
+        System.out.println("Student Added!");
+    }
+}
 public class StudentGradeManagementSystem {
     public static void main(String[] args) {
         System.out.println("Student Grade System Started");

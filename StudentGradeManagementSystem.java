049e2400804f11e38042763364159021443bd706
FARIS JM <faris.mjm2004@gmail.com>
2026-06-10 10:53:42 +0530

    Add Student class for managing student data

--- StudentGradeManagementSystem.java
+++ StudentGradeManagementSystem.java
@@ -1,3 +1,15 @@
+class Student {
+    String id;
+    String name;
+    double marks;
+
+    Student(String id, String name, double marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+}
+
 public class StudentGradeManagementSystem {
     public static void main(String[] args) {
         System.out.println("Student Grade System Started");
